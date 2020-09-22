0e9ae455d627f9baa57afbf1187aac94ef58a211
kyosheek <31596992+kyosheek@users.noreply.github.com>
2020-09-22 22:16:19 +0300

    Create new request handler

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.route('/api/whoami')
+.get((req, res) => {
+  const ipaddress = req.headers["host"],
+        language = req.headers["accept-language"],
+        software = req.headers["user-agent"];
+  res.send({ ipaddress, language, software });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Y' + listener.address().port);
